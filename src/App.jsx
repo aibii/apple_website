d963022fc5eb256388eef3a9e9436907d415d222
aibii <aybiykeomurzakova@gmail.com>
2024-10-07 21:54:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+const App = () => {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <main className="bg-black">
+      <Navbar>
+
+      </Navbar>
+      <Hero>
+
+      </Hero>
+      <Highlights>
+
+      </Highlights>
+    </main>
+  )
+}
+
+export default App
